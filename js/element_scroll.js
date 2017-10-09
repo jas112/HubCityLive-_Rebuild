81fb6e2348aedd4850f8e7a08ef10dab71723836
Jasand <jas.sandmaster@gmail.com>
2017-10-09 13:06:02 -0700

    partial reversion of style to a previous version...kept element movement and updated assets.

--- js/element_scroll.js
+++ js/element_scroll.js
@@ -1,12 +1,12 @@
 $(window).scroll(function(){
   var winScroll = $(this).scrollTop();
-  // if(winScroll > $('.gallery_images').offset().top - ($(window).height()/1.15)){
-  //   $('.gallery_images figure').each(function(i){
-  //     setTimeout(function(){
-  //       $('.gallery_images figure').eq(i).addClass('is-showing');
-  //     }, 150 * (i+1));
-  //   });
-  // }
+  if(winScroll > $('.gallery_images').offset().top - ($(window).height()/1.15)){
+    $('.gallery_images figure').each(function(i){
+      setTimeout(function(){
+        $('.gallery_images figure').eq(i).addClass('is-showing');
+      }, 150 * (i+1));
+    });
+  }
   if(winScroll > $('#aboutElementLeft').offset().top - ($(window).height()/1.15)){
     $('#aboutElementLeft').each(function(i){
       setTimeout(function(){
