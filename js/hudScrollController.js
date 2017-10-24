2def4f8f52519de00a290290be5c90d1c81ce0fa
Jasand <jas.sandmaster@gmail.com>
2017-10-24 13:32:09 -0700

    updated hud feature...created new assets...updated hud scroll action.

--- js/hudScrollController.js
+++ js/hudScrollController.js
@@ -8,9 +8,25 @@ $(window).scroll(function(){
     // 'transform': 'translate(0px, -'+ winScroll/46 +'%)'
     'transform': 'rotate(-'+ winScroll/35 +'deg)'
   });
+    $('#hudElementLeft2').css({
+    // 'transform': 'translate(0px, -'+ winScroll/46 +'%)'
+    'transform': 'rotate('+ winScroll/35 +'deg)'
+  });
+    $('#hudElementLeft3').css({
+    // 'transform': 'translate(0px, -'+ winScroll/46 +'%)'
+    'transform': 'rotate(-'+ winScroll/15 +'deg)'
+  });  
     $('#hudElementRight').css({
     // 'transform': 'translate(0px, -'+ winScroll/46 +'%)'
     'transform': 'rotate(-'+ winScroll/35 +'deg)'
+  });
+    $('#hudElementRight2').css({
+    // 'transform': 'translate(0px, -'+ winScroll/46 +'%)'
+    'transform': 'rotate('+ winScroll/35 +'deg)'
+  });
+    $('#hudElementRight3').css({
+    // 'transform': 'translate(0px, -'+ winScroll/46 +'%)'
+    'transform': 'rotate(-'+ winScroll/15 +'deg)'
   });
 
 });
