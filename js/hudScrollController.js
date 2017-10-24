42d9d10fc523a453212373c9500f7d57ff3a0811
Jasand <jas.sandmaster@gmail.com>
2017-10-23 18:10:40 -0700

    added hud element to index page with scolling action...added hud scroll controller js file.

---
+++ js/hudScrollController.js
@@ -0,0 +1,18 @@
+$(window).scroll(function(){
+  var winScroll = $(this).scrollTop();
+  console.log("winScroll = ",winScroll);
+  $('#starField1').css({
+    'transform': 'translate(0px, -'+ winScroll/3280 +'%)'
+  });
+
+
+  $('#hudElementLeft').css({
+    // 'transform': 'translate(0px, -'+ winScroll/46 +'%)'
+    'transform': 'rotate(-'+ winScroll/35 +'deg)'
+  });
+    $('#hudElementRight').css({
+    // 'transform': 'translate(0px, -'+ winScroll/46 +'%)'
+    'transform': 'rotate(-'+ winScroll/35 +'deg)'
+  });
+
+});
