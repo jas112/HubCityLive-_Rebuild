5c3623d8102b4eb4bb32ff1790b9d0e5c1f90031
Jasand <jas.sandmaster@gmail.com>
2017-10-23 20:27:05 -0700

    added hud element scroll action and element screen sizing.

--- js/hudScrollController.js
+++ js/hudScrollController.js
@@ -1,11 +1,9 @@
 $(window).scroll(function(){
   var winScroll = $(this).scrollTop();
   console.log("winScroll = ",winScroll);
-  $('#starField1').css({
-    'transform': 'translate(0px, -'+ winScroll/3280 +'%)'
-  });
-
-
+  // $('#starField1').css({
+  //   'transform': 'translate(0px, -'+ winScroll/3280 +'%)'
+  // });
   $('#hudElementLeft').css({
     // 'transform': 'translate(0px, -'+ winScroll/46 +'%)'
     'transform': 'rotate(-'+ winScroll/35 +'deg)'
