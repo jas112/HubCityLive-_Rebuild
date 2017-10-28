ff796ce8e47d4696381242ae22e8c6735a1024ab
Jasand <jas.sandmaster@gmail.com>
2017-10-27 22:29:59 -0700

    updated launch and index page bg hud elements and styling..added scroll action on the launch page.

--- js/hudScrollControllerRV.js
+++ js/hudScrollControllerRV.js
@@ -1,14 +1,14 @@
 $(window).scroll(function(){
   var winScroll = $(this).scrollTop();
   console.log("winScroll = ",winScroll);
-  $('#hudGrid0').css({
+  // $('#hudGrid0').css({
+  //   'transform': 'translate(0px, -'+ winScroll/400 +'%)'
+  // });
+  // $('#hudGridA').css({
+  //   'transform': 'translate(0px, -'+ winScroll/200 +'%)'
+  // });
+    $('#hudGridBR').css({
     'transform': 'translate(0px, -'+ winScroll/400 +'%)'
-  });
-  $('#hudGridA').css({
-    'transform': 'translate(0px, -'+ winScroll/200 +'%)'
-  });
-    $('#hudGridB').css({
-    'transform': 'translate(0px, -'+ winScroll/100 +'%)'
   });
     $('#hudCenterPoint').css({
     'transform': 'rotate(-'+ winScroll/35 +'deg)'
